9921380c1a2f964839c696c89c29ec8625f68a61
younminchan <dbsalscks0@naver.com>
2022-08-25 14:05:26 +0900

    Update RetrofitService.kt
    비어있는(length=0)인 Response를 받았을 경우 처리

--- app/src/main/java/com/example/mvvm_kotlin/network/RetrofitService.kt
+++ app/src/main/java/com/example/mvvm_kotlin/network/RetrofitService.kt
@@ -1,11 +1,14 @@
 package com.example.mvvm_kotlin.network
 
 import com.example.mvvm_kotlin.data.PhotoDataItem
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.lang.reflect.Type
 
 interface RetrofitService {
 
@@ -23,11 +26,22 @@ interface RetrofitService {
             if (networkService == null) {
                 val retrofit = Retrofit.Builder()
                     .baseUrl(baseUrl)
+                    .addConverterFactory(nullOnEmptyConverterFactory)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
                 networkService = retrofit.create(RetrofitService::class.java)
             }
             return networkService!!
+        }
+
+        /** 비어있는(length=0)인 Response를 받았을 경우 처리 */
+        private val nullOnEmptyConverterFactory = object : Converter.Factory() {
+            fun converterFactory() = this
+            override fun responseBodyConverter(type: Type, annotations: Array<out Annotation>, retrofit: Retrofit) = object :
+                Converter<ResponseBody, Any?> {
+                val nextResponseBodyConverter = retrofit.nextResponseBodyConverter<Any?>(converterFactory(), type, annotations)
+                override fun convert(value: ResponseBody) = if (value.contentLength() != 0L) nextResponseBodyConverter.convert(value) else null
+            }
         }
     }
 }
